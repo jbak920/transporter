1cede7775f4dd3f95b39912e15343e24d0934a49
Jonathan Baker <jbak920@gmail.com>
2021-04-28 17:11:28 -0600

    Changing metadata to be for Transporter

---
+++ src/main/java/com/github/jbak920/transporter/Transporter.java
@@ -0,0 +1,7 @@
+package com.github.jbak920.transporter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class Transporter {
+}
---
+++ src/main/java/com/github/jbak920/transporter/Utils.java
@@ -0,0 +1,5 @@
+package com.github.jbak920.transporter;
+
+public class Utils {
+    public static final String MOD_ID = "transporter";
+}
